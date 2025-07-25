005435662fcf802e6d2d47ec09d34c35dfe02835
kevcabanar <kevcabanar@gmail.com>
2025-07-24 23:24:49 -0500

    amigo secreto challenge 1

---
+++ README.md
@@ -0,0 +1,22 @@
+# challenge_amigo_secreto
+Projecto de nivel basico, juego del amigo secreto.
+*instrucciones de juego*
+
+## Tabla de Contenido
+- [Instalación](#instalación)
+- [Uso](#uso)
+- [Contribución](#contribución)
+
+## Instalación
+
+Para este projecto solo es necesario poder descargar el projecto y abrir el html en el navegador haciendo dovle click en index.html  open with/abrir con el navegador de preferencia.  
+Ejemplo como descargar de github:
+```bash
+git clone https://github.com/kevcabanar/challenge_amigo_secreto.git
+```
+## Uso
+Ingresar minimo 2 nombres para sortear tu amigo secreto.
+Si deseas jugar nuevamente al finalizar el sorteo, agrega nuevos amigos.
+
+## Contribución
+Esto me ayuda a estar capacitandome y mejorar mi logica de programacion y tambien poder brindar un referente a algun otro developer
--- app.js
+++ app.js
@@ -1,1 +1,64 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
+//  Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaAmigosSecretos = [];
+
+function agregarAmigo() {
+    if (inputVacio()) {
+        return;
+    } else {
+        if (listaAmigosSecretos.length === 0) {
+            reiniciarResultado();
+        }
+        let nuevoAmigo = document.getElementById("txtAmigo").value.trim();
+        if (validarExistencia(nuevoAmigo)) {
+            alert("El amigo secreto ya existe");
+        } else {
+            listaAmigosSecretos.push(nuevoAmigo);
+            document.querySelector("#txtAmigo").value = "";
+            let htmlNuevoAmigo = `<li>${nuevoAmigo}</li>`;
+            document.querySelector("#listaAmigos").innerHTML += htmlNuevoAmigo;
+            document.querySelector("#txtAmigo").focus();
+        }
+    }
+}
+
+function validarExistencia(nuevoAmigo) {
+    return listaAmigosSecretos.some(amigo => amigo === nuevoAmigo);
+}
+
+function inputVacio() {
+    let estaVacio = false;
+    let input = document.querySelector("#txtAmigo");
+    if (input.value.trim() === "") {
+        alert("Porfavor, ingrese un nombre valido");
+        input.value = "";
+        input.focus();
+        reiniciarResultado();
+        estaVacio = true;
+        return estaVacio;
+    }
+    return estaVacio;
+}
+
+function sortearAmigo() {
+    if (listaAmigosSecretos.length < 2) {
+        reiniciarResultado();
+        alert("Ingresar por lo menos 2 amigos secretos para jugar");
+    } else {
+        let amigoSeleccionado = listaAmigosSecretos[Math.floor(Math.random() * listaAmigosSecretos.length)];
+        let mensaje = `El amigo secreto sorteado es: ${amigoSeleccionado}`;
+        document.querySelector("#resultado").innerHTML = mensaje;
+        reinicioJuego();
+    }
+
+}
+
+function reinicioJuego() {
+    listaAmigosSecretos = [];
+    document.querySelector("#listaAmigos").innerHTML = "";
+}
+
+function reiniciarResultado() {
+    document.querySelector("#resultado").innerHTML = "";
+}
